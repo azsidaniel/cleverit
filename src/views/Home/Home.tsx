4a583dc64e02b18ad54004291918fd32c5bca872
Daniel Silva <danielsilva@MacBook-Air-de-Daniel.local>
2023-04-02 21:06:35 -0300

    add: eslint, editorconfig and prettier config

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,11 +1,41 @@
-import { useState } from "react";
-import { SearchInput } from "../../components/SearchInput/SearchInput";
-import { Subtitle } from "../../components/Subtitle/Subtitle";
-import { Title } from "../../components/Title/Title";
-import { HomeStyled } from "./Home.styled";
+import { useEffect, useState } from 'react';
+import { SearchInput } from '../../components/SearchInput/SearchInput';
+import { Subtitle } from '../../components/Subtitle/Subtitle';
+import { Title } from '../../components/Title/Title';
+import { HomeStyled } from './Home.styled';
+
+type Movie = {
+  coverImage: string;
+  description: string;
+  director: string;
+  id: string;
+  title: string;
+  year: number;
+};
 
 export const Home = () => {
+  const [moviesRaw, setMoviesRaw] = useState<undefined | Movie[]>();
+  const [filteredMovies, setFilteredMovies] = useState<undefined | Movie[]>();
   const [searchInput, setSearchInput] = useState<string | undefined>();
+
+  const fetchMovies = () => {
+    fetch('http://localhost:8080/movies')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setMoviesRaw(data);
+        setFilteredMovies(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchMovies();
+
+    return () => {
+      setMoviesRaw(undefined);
+    };
+  }, []);
 
   return (
     <HomeStyled>
